ad24986c0d71c887003f88ae171dac2ba7daa107
Bodaciouslycrazy <bodiemalik@gmail.com>
2024-12-30 11:45:42 -0600

    white font, better scale, github link

--- src/Contact.js
+++ src/Contact.js
@@ -2,9 +2,11 @@ function Contact(){
     return (
         <div id="contact">
             <h1>Contact</h1>
+            <br/>
 
             <p>Email: <a href="mailto:bodiemalik@gmail.com?">bodiemalik@gmail.com</a></p>
             <p>Linkedin: <a href="https://www.linkedin.com/in/bodie-malik/">linkedin.com/in/bodie-malik/</a></p>
+            <p>Github: <a href="https://github.com/Bodaciouslycrazy">github.com/Bodaciouslycrazy</a></p>
         </div>
     )
 }
