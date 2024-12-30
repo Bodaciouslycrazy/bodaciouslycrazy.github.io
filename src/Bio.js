c816e1272d8b95cfb4c12f05d7403769604eb814
Bodaciouslycrazy <bodiemalik@gmail.com>
2024-12-30 10:42:39 -0600

    minor layout tweaks

--- src/Bio.js
+++ src/Bio.js
@@ -5,7 +5,7 @@ function Bio(){
         <div id="bio" className="biopage">
             <div className="biospacer"/>
             <div className="bioinfo">
-                <img className="biopic" src={bioPic}/>
+                <img src={bioPic}/>
                 <h1>Hi, I'm Bodie!</h1>
                 <p>A software engineer and game developer with over 5 years of experience!
                     I graduated from UT Dallas with a Bachleor's in Computer Science in 2019, but my learning hasn't stopped. I continuously learn new languages and technologies every day, and I always seek new challenges with every project I work on.
--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,9 @@ function Contact(){
     return (
         <div id="contact">
             <h1>Contact</h1>
-            <p>Contact Bodie at <a href="mailto:bodiemalik@gmail.com?">bodiemalik@gmail.com</a></p>
+
+            <p>Email: <a href="mailto:bodiemalik@gmail.com?">bodiemalik@gmail.com</a></p>
+            <p>Linkedin: <a href="https://www.linkedin.com/in/bodie-malik/">linkedin.com/in/bodie-malik/</a></p>
         </div>
     )
 }
